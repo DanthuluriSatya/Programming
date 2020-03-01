ce781b9963185995c2e2bd5a6b504a9f3463a907
SatyaDanthuluri <54783024+DanthuluriSatya@users.noreply.github.com>
2020-03-01 17:23:19 -0600

    Perform push and pop operations on stack. Implement Stacks and avoid using inbuilt library.

---
+++ Implement_Stack.cpp
@@ -0,0 +1,67 @@
+/*
+Perform push and pop operations on stack. Implement Stacks and avoid using inbuilt library.
+
+Input Format
+
+First line of input contains T - number of operations. Its followed by T lines, each line contains either "push x" or "pop".
+
+Constraints
+
+1 <= T <= 10000
+-100 <= x <= 100
+
+Output Format
+
+For each "pop" operation, print the popped element, separated by newline. If the stack is empty, print "Empty".
+
+Sample Input 0
+
+8
+push 5
+pop
+pop
+push 10
+push -15
+pop
+push -10
+pop
+Sample Output 0
+
+5
+Empty
+-15
+-10
+*/
+
+Perform push and pop operations on stack. Implement Stacks and avoid using inbuilt library.
+
+Input Format
+
+First line of input contains T - number of operations. Its followed by T lines, each line contains either "push x" or "pop".
+
+Constraints
+
+1 <= T <= 10000
+-100 <= x <= 100
+
+Output Format
+
+For each "pop" operation, print the popped element, separated by newline. If the stack is empty, print "Empty".
+
+Sample Input 0
+
+8
+push 5
+pop
+pop
+push 10
+push -15
+pop
+push -10
+pop
+Sample Output 0
+
+5
+Empty
+-15
+-10
